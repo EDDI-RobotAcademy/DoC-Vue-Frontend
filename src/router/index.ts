17290b561348a012420c634671a84d5c6afc81e4
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2024-06-16 22:30:39 +0900

    add: [최민지] Home Page 추가 [PROJ-VUE-ADD-1]

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,8 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeRoutes from '@/home/router/HomeRoutes'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  ...HomeRoutes,
 ]
 
 const router = createRouter({
