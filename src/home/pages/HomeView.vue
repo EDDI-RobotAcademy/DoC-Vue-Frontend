e5e87121b14b07941f1c781911f03399daa05459
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2024-06-16 22:05:47 +0900

    chore: [최민지] vue page .gitignore 수정
    .json 추가했습니다.

--- src/home/pages/HomeView.vue
+++ src/home/pages/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
-  <hello-world />
+  <h1 align="center">이모티콘 판매 홈페이지입니다 </h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
   },
 });
 </script>
