f4c4c690b0c856048fbdc1b72848b7518ef11cbb
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2024-06-16 22:09:07 +0900

    feat: [최민지] Home Page 추가 [PROJ-VUE-ADD-1]

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,18 @@
 <template>
-  <h1 align="center">이모티콘 판매 홈페이지입니다 </h1>
+  <hello-world />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 // Components
+import HelloWorld from '../components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
+    HelloWorld,
   },
 });
 </script>
