5d591992910fbb80b3660020ca207d2ed8249b4b
younghyen7956 <155882166+younghyen7956@users.noreply.github.com>
2024-06-18 16:51:47 +0900

    feat: [김용현] 홈 페이지에 접속합니다 [PROJ-VUE-10]

---
+++ src/navigationBar/NavigationMenuBar.vue
@@ -0,0 +1,66 @@
+<template>
+    <v-app-bar color="orange" app dark height="50">
+      <v-container class="d-flex justify-center align-center">
+        <v-btn @click="goToHome" class="mx-auto">
+          <v-toolbar-title class="text-uppercase text--darken-4">
+            <span>이모티콘 project</span>
+          </v-toolbar-title>
+        </v-btn>
+      </v-container>
+    </v-app-bar>
+  
+    <v-app-bar color="primary" app dark height="50">
+      <v-container class="d-flex justify-center">
+        <v-btn text @click="goToProductList" class="btn-text">
+          <v-icon left>mdi-store</v-icon>
+          <span>상품</span>
+        </v-btn>
+        <v-btn text @click="goToBoardList" class="btn-text">
+          <v-icon left>mdi-forum</v-icon>
+          <span>게시판</span>
+        </v-btn>
+        <v-btn v-if="!isLogin" text @click="signIn" class="btn-text">
+          <v-icon left>mdi-login</v-icon>
+          <span>로그인</span>
+        </v-btn>
+        <v-btn v-if="isLogin" text @click="signOut" class="btn-text">
+          <v-icon left>mdi-logout</v-icon>
+          <span>로그아웃</span>
+        </v-btn>
+      </v-container>
+    </v-app-bar>
+  </template>
+
+<script>
+import '@mdi/font/css/materialdesignicons.css'
+import router from '@/router'
+
+export default {
+    data () {
+        return {
+            navigation_drawer: false,
+            // links: [{ icon: 'mdi-home', action: this.goToHome, route: '/' }],
+            accessToken: null,
+            isLogin: false,
+            // items: [
+            //     { title: 'Product', action: this.goToProductList() },
+            //     { title: 'Board', action: this.goToBoardList() },
+            // ]
+        }
+    },
+    methods: {
+        goToHome () {
+            router.push('/')
+        },
+        goToProductList () {
+            router.push('/product/list')
+        },
+        goToBoardList () {
+            router.push('/board/list')
+        },
+        signIn () {
+            router.push('/account/login')
+        }
+    }
+}
+</script>
