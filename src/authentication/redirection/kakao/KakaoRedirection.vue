8bd90f18b71e2c803ba58d9040ef1716b0ce2bfc
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2024-06-26 15:28:57 +0900

    add: [최민지] 기존, 신규 로그인 시 alert로 알림
    너무  휙휙 넘어가서 alert()로 알림 기능 추가했습니다. 비록 경고를 위한 기능이지만..

--- src/authentication/redirection/kakao/KakaoRedirection.vue
+++ src/authentication/redirection/kakao/KakaoRedirection.vue
@@ -29,6 +29,7 @@ export default {
 
             if (isEmailDuplication === true) {
                 console.log('기존 가입 고객입니다!')
+                alert('로그인에 성공하였습니다!')
                 const accessToken = localStorage.getItem("accessToken");
 
                 if (accessToken) {
@@ -40,6 +41,7 @@ export default {
                 this.$router.push('/')
             } else {
                 console.log('신규 가입 고객입니다!')
+                alert('신규 가입 고객입니다!')
 
                 this.$router.push('/account/register')
             }
