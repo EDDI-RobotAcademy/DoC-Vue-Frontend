78db870b5ddaec631ed07f2e4f8cd7bb0fc88a1a
y0ng98 <dldydgnl2080@gmail.com>
2024-07-04 17:48:26 +0900

    feat: [이용휘] 나의 주문 내역 확인 가능 [PROJ-VUE-32]

---
+++ src/order/router/OrderRoutes.ts
@@ -0,0 +1,11 @@
+import OrderListPage from '@/order/pages/list/OrderListPage.vue'
+
+const OrderRoutes = [
+    {
+        path: '/order/list',
+        name: 'OrderListPage',
+        component: OrderListPage,
+    }
+]
+
+export default OrderRoutes
--- src/order/store/states.ts
+++ src/order/store/states.ts
@@ -1,19 +1,29 @@
 export interface OrderState {
     orderItemList: OrderItem[]
     orderItem: OrderItem | null
+    orderList: Order[]
+    order: Order | null 
 }
 
 export interface OrderItem {
-    orderId: number
+    orderItemId: number
     productId: number
     cartItemId: number
     quantity: number
     price: number
 }
 
+export interface Order {
+    orderId: number
+    orderData: string
+    orderTotalPrice: number
+}
+
 const state: OrderState = {
     orderItemList: [],
-    orderItem: null
+    orderItem: null,
+    orderList: [],
+    order: null,
 }
 
 export default state
